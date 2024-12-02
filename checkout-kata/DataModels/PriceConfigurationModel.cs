8ebb5ef804cf7f35064510d56b7e135a846e22c6
shauna <91smcknight@gmail.com>
2024-12-02 23:24:56 +0000

    Added simplified functionality for scan and get total Updated tests to cover new functionality Removed MSTest and added Nunit

---
+++ checkout-kata/DataModels/PriceConfigurationModel.cs
@@ -0,0 +1,8 @@
+﻿namespace checkout_kata.DataModels
+{
+    public class PriceConfigurationModel
+    {
+        public int Price { get; set; }
+        public string SKU { get; set; }
+    }
+}
--- checkout-kataTests/CheckoutApplicationTests.cs
+++ checkout-kataTests/CheckoutApplicationTests.cs
@@ -1,14 +1,74 @@
 ﻿using checkout_kata;
+using NUnit.Framework;
+using System;
+using checkout_kata.DataModels;
 
 namespace checkout_kataTests
 {
-    [TestClass()]
+    [TestFixture]
     public class CheckoutApplicationTests
     {
-        [TestMethod()]
-        public void GivenInitialSetup_WhenProcessRan_ThenThrowNotImplementedException()
+        private Checkout _checkout;
+
+        [SetUp]
+        public void Init()
         {
-            Assert.ThrowsException<NotImplementedException>(() => CheckoutApplication.Process("ABC"));
+            _checkout = new Checkout();
+        }
+
+        [Test]
+        [TestCase("ABCM")]
+        [TestCase("ABCZ")]
+        [TestCase("TCA")]
+        public void GivenInvalidSku_WhenProcessRan_ThenThrowKeyNotFoundException(string skus)
+        {
+            //Arrange
+            //Act
+            //Assert
+            Assert.That(() =>
+            {
+                foreach (var sku in skus)
+                {
+                    _checkout.Scan(sku.ToString());
+                }
+            },
+                Throws.TypeOf<KeyNotFoundException>()
+                    .With.Message.EqualTo($"Scanned SKU does not exist."));
+        }
+
+        [Test]
+        [TestCase("AB")]
+        [TestCase("ABC")]
+        [TestCase("AAAAAA")]
+        public void GivenValidSku_WhenScan_ThenAddToScannedItems(string skus)
+        {
+            //Arrange
+            //Act
+            foreach (var sku in skus)
+            {
+                _checkout.Scan(sku.ToString());
+            }
+
+            //Assert
+            Assert.That(_checkout.scannedItemCount, Is.Not.Null);
+        }
+
+        [Test]
+        [TestCase("ABC")]
+        public void GivenValidSku_WhenGetTotalCount_ThenReturnCorrectTotalPrice(string skus)
+        {
+            //Arrange
+            //Act
+            foreach (var sku in skus)
+            {
+                _checkout.Scan(sku.ToString());
+            }
+
+            var totalPrice = _checkout.GetTotalPrice();
+
+            //Assert
+            Assert.That(totalPrice, Is.Not.Null);
+            Assert.That(totalPrice, Is.EqualTo(100));
         }
     }
 }
